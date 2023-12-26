0f2ce8424c9bee8e7c95d5c077e34904cd7f82c0
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-12-26 03:27:30 +0200

    Добавьте файлы проекта.

---
+++ lab 3-2 Fomichev/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine());
+
+int result = n / m;
+int squareResult = result * result;
+
+Console.WriteLine($"Квадрат целочисленного деления {n} на {m}: {squareResult}");
